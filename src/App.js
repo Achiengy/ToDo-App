d7d39045ab0b713bef2fc9a4ad6bdcc6059b832e
+ <+>
2022-06-30 17:11:04 +0300

    Add useEffects for handling todos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 import "./css/main.css";
 import DisplayTodos from "./components/DisplayTodos";
 import Todos from "./components/Todos";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
- 
 
-import { motion } from "framer-motion";
-function App() {
+ import { motion } from "framer-motion";
+function App({history}) {
+
+  //handle todos
+  const [todo, setTodo] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/todo")
+      .then((r) => r.json())
+      .then((todo) => setTodo(todo));
+  }, []);
+
+  const fetchTodos = () => {
+    fetch('/api/todo')
+      .then((res) => res.json())
+      .then((data) => setTodo(data))
+  }
+
+  const patchTodos = (todo) => {
+    fetch(`/api/todo/${todo.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+    })
+  }
+
+  const postTodos = (todo) => {
+    fetch('/api/todo/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+      body: JSON.stringify(todo),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTodo((prevTodo) => {
+          return [...prevTodo, data]
+        })
+      })
+  }
+
+
+
+
+
+
+
+
   return (
     <div className="App">
-   
-        <nav>
+      <nav>
         <motion.h1
         initial={{ y: -200 }}
         animate={{ y: 0 }}
@@ -19,7 +70,7 @@ function App() {
       >
         My.ToDo
       </motion.h1>
-        <button> LogIn </button>
+        <button onClick={() => history.push('/login')}> LogIn </button>
         </nav>
 
       <motion.div
